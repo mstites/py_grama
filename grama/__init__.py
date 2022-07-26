a95c141d78828e922f48be59e278fd73542b0b9b
mstites <maeve@mstites.com>
2022-07-26 13:53:24 -0400

    Revert "Remove last of non dfply"
    This reverts commit 8d0483addac3915c5ffa18281d9af24e8a7ef69d.

--- grama/__init__.py
+++ grama/__init__.py
@@ -1,3 +1,15 @@
+# Import core
+# --------------------------------------------------
+from .tools import *
+from .core import *
+
 # Import dfply
 # --------------------------------------------------
 from .dfply import *
+
+# Marginals uses make_symbolic decorator
+from .marginals import *
+
+## Load plotnine
+# --------------------------------------------------
+from plotnine import *
