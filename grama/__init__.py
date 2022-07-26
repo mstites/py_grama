8d0483addac3915c5ffa18281d9af24e8a7ef69d
mstites <maeve@mstites.com>
2022-07-26 13:50:42 -0400

    Remove last of non dfply

--- grama/__init__.py
+++ grama/__init__.py
@@ -1,15 +1,3 @@
-# Import core
-# --------------------------------------------------
-from .tools import *
-from .core import *
-
 # Import dfply
 # --------------------------------------------------
 from .dfply import *
-
-# Marginals uses make_symbolic decorator
-from .marginals import *
-
-## Load plotnine
-# --------------------------------------------------
-from plotnine import *
